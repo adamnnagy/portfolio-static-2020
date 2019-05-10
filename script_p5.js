46b10e34fdc10a37d683c54d1ff44815c026a96c
Adam Nagy <an2208@nyu.edu>
2019-05-10 23:28:45 +0200

    Improving code

---
+++ script_p5.js
@@ -0,0 +1,63 @@
+//centers the canvas
+function centerCanvas(canvas) {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  canvas.position(x, y);
+  return canvas;
+}
+
+
+
+function windowResized() {
+	resize();
+}
+
+function customSetup(){
+	background(255);
+}
+
+
+function resize(){
+
+  var w = select('#canvas-container').width;
+  var h = select('#canvas-container').height;
+
+  console.log(w + " : " + h);
+
+  // resizeCanvas(w, h);
+  resizeCanvas(windowWidth, windowHeight);
+
+}
+
+//###################################
+//##########ACTUAL SKETCH############
+//###################################
+
+function setup() {
+
+  var cnv;
+  var x = (windowWidth) / 2;
+  var y = (windowHeight) / 2;
+
+  cnv = createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+  //cnv = centerCanvas(cnv);
+  cnv.position(x,y);
+  cnv.parent("canvas-container");
+
+}
+
+function draw() {
+  background(220);
+
+  push();
+  rectMode(CENTER);
+  rotateX(-0.5);
+  rotateY((frameCount + (mouseX-width/2) )* 0.003);
+  noFill();
+  strokeWeight(2);
+  smooth();
+  box(200, 200, 200, 1, 1);
+  pop();
+
+
+}
